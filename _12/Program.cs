70bf1e3187140d640478110ce963f2cf1eac8826
Kirill Miller <belogorski2000@gmail.com>
2022-08-27 12:19:43 +0300

    Добавьте файлы проекта.

---
+++ _12/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус окружности.");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Кординаты центра окружности х0, у0.");
+            double x0 = Convert.ToDouble(Console.ReadLine());
+            double y0 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите кординаты точки.");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            double Circumference = Rectangle.GetCircumference(r);
+            double Square = Rectangle.GetSquare(r);
+            double Have = Rectangle.GetHave(r, x0, y0, x1, y1);
+            Console.WriteLine($"{Circumference}");
+            Console.WriteLine($"{Square}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ _12/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    static class Rectangle
+    {
+        static public double GetCircumference(double r)
+        {
+            return Math.PI * r * r;
+        }
+        static public double GetSquare(double r)
+        {
+            return 2.0 * Math.PI * r;
+        }
+        static public double GetHave(double r, double x0, double y0, double x1, double y1)
+        {
+            if ((x1 > x0 + r) & (x1 < x0 - r) & (y1 > y0 + r) & (y1 < y0 - r))
+                Console.WriteLine("sc");
+            else
+                Console.WriteLine("snknc");
+        }
+    }
+}
